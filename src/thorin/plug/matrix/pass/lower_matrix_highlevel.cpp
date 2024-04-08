0d051004b8f738e0e098572bed731c325c35ba7d
juhigupta0 <juhigupta262@gmail.com>
2024-04-08 11:55:51 +0200

    Introducing demonstration of merging operator

--- src/thorin/plug/matrix/pass/lower_matrix_highlevel.cpp
+++ src/thorin/plug/matrix/pass/lower_matrix_highlevel.cpp
@@ -51,7 +51,52 @@ Ref LowerMatrixHighLevelMapRed::rewrite_(Ref def) {
             auto fun_app = world().app(ds_fun, {mem, m, k, l, M, N});
             return fun_app;
         }
+    } else if (auto mat_ax = match<matrix::multiplication_gpu>(def)) {
+        auto [mem, M, N, O]  = mat_ax->args<4>();
+        auto [m, k, l, w] = mat_ax->decurry()->args<4>();
+        auto w_lit        = Lit::isa(w);
+
+        auto ext_fun = world().external(world().sym("extern_matrix_multiplication_gpu"));
+        if (ext_fun && (w_lit && *w_lit == 64)) {
+            auto ds_fun  = direct::op_cps2ds_dep(ext_fun);
+            auto fun_app = world().app(ds_fun, {mem, m, k, l, M, N, O});
+            return fun_app;
+        }
+    } else if (auto mat_ax = match<matrix::addition>(def)) {
+        auto [mem, M, N]  = mat_ax->args<3>();
+        auto [m, k, w] = mat_ax->decurry()->args<3>();
+        auto w_lit        = Lit::isa(w);
+
+        auto ext_fun = world().external(world().sym("extern_matrix_addition"));
+        if (ext_fun && (w_lit && *w_lit == 64)) {
+            auto ds_fun  = direct::op_cps2ds_dep(ext_fun);
+            auto fun_app = world().app(ds_fun, {mem, m, k, M, N});
+            return fun_app;
+        }
+    } else if (auto mat_ax = match<matrix::addition_gpu>(def)) {
+        auto [mem, M, N, O]  = mat_ax->args<4>();
+        auto [m, k, w] = mat_ax->decurry()->args<3>();
+        auto w_lit        = Lit::isa(w);
+
+        auto ext_fun = world().external(world().sym("extern_matrix_addition_gpu"));
+        if (ext_fun && (w_lit && *w_lit == 64)) {
+            auto ds_fun  = direct::op_cps2ds_dep(ext_fun);
+            auto fun_app = world().app(ds_fun, {mem, m, k, M, N, O});
+            return fun_app;
+        }
+    } else if (auto mat_ax = match<matrix::add_multiply_gpu>(def)) {
+        auto [mem, A, B, C, O]  = mat_ax->args<5>();
+        auto [m, w] = mat_ax->decurry()->args<2>();
+        auto w_lit        = Lit::isa(w);
+
+        auto ext_fun = world().external(world().sym("extern_matrix_add_multiply_gpu"));
+        if (ext_fun && (w_lit && *w_lit == 64)) {
+            auto ds_fun  = direct::op_cps2ds_dep(ext_fun);
+            auto fun_app = world().app(ds_fun, {mem, m, A, B, C, O});
+            return fun_app;
+        }
     }
+    
 
     if (auto outer_app = def->isa<App>()) {
         if (auto inner_app = outer_app->callee()->isa<App>()) {
